fdc2e2126902205d80e7ebd1757ba3859e4385c6
woodstock-pi <wdstck.pi@gmail.com>
2024-11-22 14:09:43 -0300

    "Atualização da mensagem"

--- check.py
+++ check.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     print("Enviando IPs para o Telegram...")
     message = (
-        f"Raspberry pi CONECTADO!\n\n"
+        f"Raspberry pi CONECTADO!!\n\n"
         f"IP Local: {local_ip}\n"
         f"IP Externo: {external_ip}"
     )
